c5e7106cf1f27aad5a0d6917b54a47f0e59ca77c
Raoul Goorden <rgoorden@t-mobilethuis.nl>
2022-04-04 15:11:51 +0200

    did something
    1st setup of front-end

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'http://localhost:8081/api',
+  USER_API_URL: 'https://localhost:44335'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  API_URL: 'http://localhost:8081/api',
+  USER_API_URL: 'https://localhost:44335'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
